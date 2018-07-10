7f74111195b49db1514e73dff9424a7b8269ea7c
Michael Chung <mikechung@live.com>
2018-07-10 09:43:17 -0700

    Modify statcalc.csproj

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -440,7 +440,7 @@ _angular_platform_browser__WEBPACK_IMPORTED_MODULE_3__["platformBrowser"]().boot
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! G:\Code\repos\projects\StatCalc\statcalc\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Projects_test\MFF\StatCalc\StatCalc\src\main.ts */"./src/main.ts");
 
 
 /***/ })
